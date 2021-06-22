4ee940343a606065decf590d9483b2f1a3821825
markital <markital@ukr.net>
2021-06-22 20:58:29 +0300

    Я зробив зміни 5

---
+++ main.js
@@ -0,0 +1,49 @@
+
+//Візьміть файл template1.html, підключіть до нього скрипт, і працюйте в ньому.
+
+//1) Напишіть код,  який за допомоги document.getElementById або document.getElementsByClassName або document.getElementsByTagName :
+   // a) отримує текст з параграфа з id "content"
+   //let content=document.getElementById('content')
+   //console.log('content');
+
+    //b) отримує текст з блоку з id "rules"
+    //let ElementsByClassName=document.getElementsByClassName('rules');
+      //console.log(ElementsByClassName);
+
+   // c) замініть текст параграфа з id 'content' на будь-який інший
+   //let content=document.getElementById('content')
+  // content.innerText='text'
+   //console.log(content);
+
+    //d) замініть текст параграфа з id 'rules' на будь-який інший
+    //let ElementsByClassName=document.getElementsByClassName('rules');
+    //rules.innerText='1234'
+    //console.log(ElementsByClassName);
+
+    //e) змініть кожному елементу колір фону на червоний
+//let elem = document.body.getElementsByTagName('*');
+//for (const item of elem) {
+   //item.style.background = 'red'
+   
+
+
+    //f) змініть кожному елементу колір тексту на синій
+     //let elem = document.body.getElementsByTagName('*');
+//for (const item of elem) {
+   //item.style.color='blue'}
+
+
+
+    //g) отримати весь список класів елемента з id=rules і вивести їх в console.log
+    //let dim = document.getElementsByClassName('rules')
+    //console.log(rules);
+
+    //h) отримати всі елементи з класом fc_rules    
+    // i) поміняти колір тексту у всіх елементів fc_rules на червоний
+
+    //let dim = document.getElementsByClassName('fc_rules')
+
+        //for (const item of dim) {
+     // item.style.color='red'}
+      
+
